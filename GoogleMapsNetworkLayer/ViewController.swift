510d9d372045ba2716092f4e7609e227782d05d2
Ahmed Meguid <ameguid12@gmail.com>
2018-08-11 14:54:12 +0200

    xcode project init

---
+++ GoogleMapsNetworkLayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleMapsNetworkLayer
+//
+//  Created by Ahmed Abdel Meguid on 8/11/18.
+//  Copyright © 2018 Ahmed Abdel Meguid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
